85fe2652d258c3871d1064c02a9daa20fd1e7c32
ColeRutledge <ColeRutledge@gmail.com>
2020-10-29 17:00:02 -0400

    adds server side rendering for posts

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -3,6 +3,8 @@ import { MyContext } from "src/types"
 import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql"
 import argon2 from 'argon2'
 import { EntityManager } from '@mikro-orm/postgresql'
+import { COOKIE_NAME } from "../constants"
+
 
 @InputType()
 class UsernamePasswordInput {
@@ -131,5 +133,19 @@ export class UserResolver {
         return {
             user,
         }
+    }
+
+    @Mutation(() => Boolean)
+    logout(@Ctx() { req, res }: MyContext) {
+        return new Promise(resolve => req.session.destroy(err => {
+            res.clearCookie(COOKIE_NAME)
+            if (err) {
+                console.log(err)
+                resolve(false)
+                return
+            }
+
+            resolve(true)
+        }))
     }
 }
